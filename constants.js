9ca7c137aa382c78109fcef8714259f13c061fde
bereket-abera <bereket5042@gmail.com>
2020-01-11 12:43:31 +0300

    add contants file

---
+++ constants.js
@@ -0,0 +1,17 @@
+// const path = require('path');
+
+
+const baseDir = __dirname;
+
+const JWTSECRET = "ad4f783b-bbc9-43a1-88a4-1bec8da0f363";
+
+const JWTEMAILSECRET = "7e439511-9b12-4605-9283-256886c53e14";
+
+const JWTPASSWORDSECRET = "4ddb245c-28c8-4f79-a1e0-2488531719ec ";
+
+module.exports = {
+    baseDir,
+    JWTSECRET,
+    JWTEMAILSECRET,
+    JWTPASSWORDSECRET
+}
