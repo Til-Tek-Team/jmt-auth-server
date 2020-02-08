dc81af8678cc51031293c55f8b4fdab2930c75c1
Silas <silogecho@gmail.com>
2020-02-08 11:12:50 +0300

    merged

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -181,22 +181,6 @@ async function buyPlanHandler(data) {
     subscription["expirationDate"] = null;
     subscription["premiumType"] = null;
   } else {
-<<<<<<< HEAD
-    subscription["expirationDate"] = subscription.expirationDate
-      ? new Date(
-        new Date(subscription.expirationDate).getTime() +
-        24 * 60 * 60 * 1000 * payType.valueInDays
-      )
-        .toISOString()
-        .split(".")[0]
-        .replace("T", " ")
-      : new Date(
-        new Date().getTime() + 24 * 60 * 60 * 1000 * payType.valueInDays
-      )
-        .toISOString()
-        .split(".")[0]
-        .replace("T", " ");
-=======
     subscription["expirationDate"] =
       subscription.expirationDate &&
       subscription.expirationDate >
@@ -217,7 +201,6 @@ async function buyPlanHandler(data) {
             .toISOString()
             .split(".")[0]
             .replace("T", " ");
->>>>>>> 31fbbc709615b5a0f00fb7d25e143798eae98cca
     subscription["premiumType"] = payType.name;
     subscription["points"] = null;
     subscription["expressType"] = null;
